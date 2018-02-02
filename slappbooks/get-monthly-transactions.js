a5fd50ea6d36fb71f791ff309abdf4c1ed30da09
Malith Jayaweera <malith.12@cse.mrt.ac.lk>
2018-02-02 16:35:59 +0530

    Added license header

--- slappbooks/get-monthly-transactions.js
+++ slappbooks/get-monthly-transactions.js
@@ -1,8 +1,31 @@
+/*
+ * Copyright (c) 2018 SLAppForge Lanka (Private) Limited. All Rights Reserved.
+ * https://www.slappforge.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License. 
+ */
+
 let AWS = require('aws-sdk');
 let connectionManager = require('./ConnectionManager');
 let SL = require('@slappforge/slappforge-sdk');
 const rds = new SL.AWS.RDS(connectionManager);
 
+/**
+ * Lambda function retrieves transactions month wise while supporting pagination. Events are submitted through the application as an identifier. 
+ * An RDS instance is used for transaction retrieval.
+ *
+ * @author Malith Jayaweera
+ */
 exports.handler = function (event, context, callback) {
 
 	let postObject = event;
--- slappbooks/insert-transaction-with-currency.js
+++ slappbooks/insert-transaction-with-currency.js
@@ -1,8 +1,33 @@
+/*
+ * Copyright (c) 2018 SLAppForge Lanka (Private) Limited. All Rights Reserved.
+ * https://www.slappforge.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License. 
+ */
+
 let AWS = require('aws-sdk');
 let connectionManager = require('./ConnectionManager');
 let SL = require('@slappforge/slappforge-sdk');
 const rds = new SL.AWS.RDS(connectionManager);
 
+
+/**
+ * Lambda function handles transaction inserts. Events are submitted through the application as transaction objects. 
+ * An RDS instance is used for transaction inserts. Currency differences are also handled through this lambda function as opposed to a usual 
+ * transaction insert.Transactional behaviour is guaranteed for the insert.
+ *
+ * @author Malith Jayaweera
+ */
 exports.handler = function (event, context, callback) {
 
 	postObject = event;
