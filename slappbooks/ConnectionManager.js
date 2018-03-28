ad0e993c3ad6f193c27ffc8dc2569d87f5c0d830
Janaka Bandara <janakaud@gmail.com>
2018-03-28 15:08:27 +0530

    updating RDS configs

--- slappbooks/ConnectionManager.js
+++ slappbooks/ConnectionManager.js
@@ -25,11 +25,11 @@ module.exports = function () {
     this.dbConnections = [];
 
     this.dbConnections["slappbooksdb"] = {
-        host: process.env.EndPoint_slappbooksdb,
-        port: "3306",
-        user: "slappbooksuser",
-        password: "12345678",
-        database: "slappbooksdb",
-    };
+             host: process.env.EndPoint_rdsSlappbooksdb,
+             port: process.env.Port_rdsSlappbooksdb,
+             user: process.env.User_rdsSlappbooksdb,
+             password: process.env.Password_rdsSlappbooksdb,
+             database: "slappbooksdb",
+        };
 
 };
